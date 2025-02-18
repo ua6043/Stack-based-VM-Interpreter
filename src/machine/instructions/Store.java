8554339feb46f275d8e8b87b0d80001ebb1004b4
Umar Arif <ua6043@rit.edu>
2025-02-18 12:12:45 -0500

    Passed test for Store class

--- src/machine/instructions/Store.java
+++ src/machine/instructions/Store.java
@@ -1,23 +1,29 @@
 package machine.instructions;
 
+import common.SymbolTable;
 import machine.InstructionStack;
 import machine.Alaton;
 
 public class Store implements Instruction{
 
-
+    private final InstructionStack stack;
+    private final SymbolTable symTbl;
+    private String name;
 
     public Store(String name, Alaton machine) {
-
+        this.stack = machine.getInstructionStack();
+        this.name = name;
+        this.symTbl = machine.getSymbolTable();
     }
 
     @Override
     public void execute() {
-
+        int top = this.stack.pop();
+        this.symTbl.set(this.name, top);
     }
 
     @Override
     public String toString() {
-        return null;
+        return Alaton.STORE + " " + this.name;
     }
 }
