27dbf180ec87cc1f8556ab9508599339972255b2
Umar Arif <ua6043@rit.edu>
2025-02-18 12:41:14 -0500

    Passed test for Power class

--- src/machine/instructions/Power.java
+++ src/machine/instructions/Power.java
@@ -5,19 +5,21 @@ import machine.Alaton;
 
 public class Power implements Instruction{
 
-
+    private final InstructionStack stack;
 
     public Power(Alaton machine) {
-
+        this.stack = machine.getInstructionStack();
     }
 
     @Override
     public void execute() {
-
+        int second = this.stack.pop();
+        int first = this.stack.pop();
+        this.stack.push((int) Math.pow(first, second));
     }
 
     @Override
     public String toString() {
-        return null;
+        return Alaton.POWER;
     }
 }
