cfdbe86905732e1f4c84e09fccd69d54625733aa
Umar Arif <ua6043@rit.edu>
2025-02-18 12:02:56 -0500

    Passed test for Print class

--- src/machine/instructions/Print.java
+++ src/machine/instructions/Print.java
@@ -5,19 +5,20 @@ import machine.Alaton;
 
 public class Print implements Instruction{
 
-
+    private final InstructionStack stack;
 
     public Print(Alaton machine) {
-
+        this.stack = machine.getInstructionStack();
     }
 
     @Override
     public void execute() {
-
+        int top = this.stack.pop();
+        System.out.println(top);
     }
 
     @Override
     public String toString() {
-        return null;
+        return Alaton.PRINT;
     }
 }
