19352f7fd2a64115f0877c14f5e50793a08c215f
Umar Arif <ua6043@rit.edu>
2025-02-18 12:29:51 -0500

    Passed test for Add class

--- src/machine/instructions/Add.java
+++ src/machine/instructions/Add.java
@@ -5,19 +5,21 @@ import machine.Alaton;
 
 public class Add implements Instruction{
 
-
+    private InstructionStack stack;
 
     public Add(Alaton machine) {
-
+        this.stack = machine.getInstructionStack();
     }
 
     @Override
     public void execute() {
-
+        int second = this.stack.pop();
+        int first = this.stack.pop();
+        this.stack.push(first+second);
     }
 
     @Override
     public String toString() {
-        return null;
+        return Alaton.ADD;
     }
 }
