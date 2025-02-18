743c76e7bd2793a5d46fa011191e47ec78963089
Umar Arif <ua6043@rit.edu>
2025-02-18 12:38:58 -0500

    Passed test for Modulus class

--- src/machine/instructions/Modulus.java
+++ src/machine/instructions/Modulus.java
@@ -5,19 +5,21 @@ import machine.Alaton;
 
 public class Modulus implements Instruction{
 
-
+    private final InstructionStack stack;
 
     public Modulus(Alaton machine) {
-
+        this.stack = machine.getInstructionStack();
     }
 
     @Override
     public void execute() {
-
+        int second = this.stack.pop();
+        int first = this.stack.pop();
+        this.stack.push(first%second);
     }
 
     @Override
     public String toString() {
-        return null;
+        return Alaton.MODULUS;
     }
 }
