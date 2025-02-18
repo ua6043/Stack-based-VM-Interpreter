4354fa2456a8b3e920cd233e0a6f2e3d1cbdee45
Umar Arif <ua6043@rit.edu>
2025-02-18 12:17:53 -0500

    Passed test for Load class

--- src/machine/instructions/Load.java
+++ src/machine/instructions/Load.java
@@ -1,23 +1,29 @@
 package machine.instructions;
 
+import common.SymbolTable;
 import machine.InstructionStack;
 import machine.Alaton;
 
 public class Load implements Instruction{
 
-
+    private final InstructionStack stack;
+    private final SymbolTable symTbl;
+    private String name;
 
     public Load(String name, Alaton machine) {
-
+        this.name = name;
+        this.stack = machine.getInstructionStack();
+        this.symTbl = machine.getSymbolTable();
     }
 
     @Override
     public void execute() {
-
+        int value = this.symTbl.get(this.name);
+        this.stack.push(value);
     }
 
     @Override
     public String toString() {
-        return null;
+        return Alaton.LOAD + " " + this.name;
     }
 }
