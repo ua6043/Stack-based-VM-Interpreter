063f0d2d61e2542dbc2cba7371a242fea8ce3005
Umar Arif <ua6043@rit.edu>
2025-02-18 12:27:00 -0500

    Passed test for SquareRoot class

--- src/machine/instructions/SquareRoot.java
+++ src/machine/instructions/SquareRoot.java
@@ -5,19 +5,20 @@ import machine.Alaton;
 
 public class SquareRoot implements Instruction{
 
-
+    private final InstructionStack stack;
 
     public SquareRoot(Alaton machine) {
-
+        this.stack = machine.getInstructionStack();
     }
 
     @Override
     public void execute() {
-
+        int top = this.stack.pop();
+        this.stack.push((int) Math.sqrt(top));
     }
 
     @Override
     public String toString() {
-        return null;
+        return Alaton.SQUARE_ROOT;
     }
 }
