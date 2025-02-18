741611a5f9d0447f7d5869bdb896f988dd4494d2
Umar Arif <ua6043@rit.edu>
2025-02-18 12:36:56 -0500

    Passed test for Divide class

--- src/machine/instructions/Divide.java
+++ src/machine/instructions/Divide.java
@@ -5,19 +5,21 @@ import machine.Alaton;
 
 public class Divide implements Instruction{
 
-
+    private final InstructionStack stack;
 
     public Divide(Alaton machine) {
-
+        this.stack = machine.getInstructionStack();
     }
 
     @Override
     public void execute() {
-
+        int second = this.stack.pop();
+        int first = this.stack.pop();
+        this.stack.push(first/second);
     }
 
     @Override
     public String toString() {
-        return null;
+        return Alaton.DIVIDE;
     }
 }
